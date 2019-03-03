fa5d8f0bdceb48daa68b2a39b8c116a393125ce4
umang <umang.master88@gmail.com>
2019-03-03 15:32:56 +0530

    first commit

---
+++ blog_project/blog/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post as post_model
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from taggit.models import Tag
+
+# Create your views here.
+def post_list_view(request,tag_slug=None):
+    post_list=post_model.objects.all()
+    tag=None
+    if tag_slug:
+        tag=get_object_or_404(Tag,slug=tag_slug)
+        post_list=post_list.filter(tags__in=[tag])
+    paginator=Paginator(post_list,3)
+    page_number=request.GET.get('page')
+    try:
+        post_list=paginator.page(page_number)
+    except PageNotAnInteger:
+        post_list=paginator.page(1)
+    except EmptyPage:
+        post_list=paginator.page(paginator.num_pages)
+    return render(request,'blog/post_list.html',{'post_list':post_list,'tag':tag})
+
+
+from blog.forms import CommentForm
+def post_detail_view(request,year,month,day,post):
+    post=get_object_or_404(post_model,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+    comments=post.comments.filter(active=True)
+    csubmit=False
+    if request.method=='POST':
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            new_comment=form.save(commit=False)
+            new_comment.post=post
+            new_comment.save()
+            csubmit=True
+    else:
+        form=CommentForm()
+    return render(request,'blog/post_detail.html',{'post':post,'form':form,'csubmit':csubmit})
+
+
+from django.core.mail import send_mail
+from blog.forms import EmailSendForm
+
+def mail_send_view(request,id):
+    post=get_object_or_404(post_model,id=id,status='published')
+    sent=False
+    if request.method=='POST':
+        form=EmailSendForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            subject='{} ({}) recommended you to read"{}"'.format(cd['name'],cd['email'],post.title)
+            post_url=request.build_absolute_uri(post.get_absolute_url())
+            message='Read post at:\n {}\n\n{}\'s comments:\n{}'.format(post_url,cd['name'],cd['comments'])
+            send_mail(subject,message,'umang.master92@gmail.com',[cd['to']])
+            sent=True
+    else:
+        form=EmailSendForm()
+    return render(request,'blog/sharebymail.html',{'form':form,'post':post,'sent':sent})
